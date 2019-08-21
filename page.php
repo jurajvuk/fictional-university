865e629fce3e35713f4a7c878faa53ceb4e8264d
jurajvuk <jurajvuk@gmail.com>
2019-08-21 15:36:24 +0200

    second commit

--- page.php
+++ page.php
@@ -2,9 +2,53 @@
 <?php
     while (have_posts()) {
         the_post(); ?>
-        <h1>This is a Page</h1>
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
+        
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+                <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title(); ?></h1>
+                <div class="page-banner__intro">
+                    <p>DON'T FORGET TO REPLACE ME LATER</p>
+                </div>
+            </div>  
+        </div>
+
+        <div class="container container--narrow page-section">
+            <?php $theParent = wp_get_post_parent_ID(get_the_ID()); ?>
+            <?php if($theParent) : ?>
+                <div class="metabox metabox--position-up metabox--with-home-link">
+                    <p><a class="metabox__blog-home-link" href="<?php the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+                </div>
+            <?php endif; ?>
+            
+            <?php 
+                $testArray = get_pages(array(
+                    'child_of' => get_the_ID()
+                ));
+            ?>
+            <?php if($theParent or $testArray) : ?>
+                <div class="page-links">
+                    <h2 class="page-links__title"><a href="<?php the_permalink($theParent); ?>"><?php echo get_the_title($theParent) ?></a></h2>
+                    <ul class="min-list">
+                        <?php
+                            if ($theParent) {
+                                $findChildrenOf = $theParent;
+                            } else {
+                                $findChildrenOf = get_the_ID();
+                            }
+                            wp_list_pages(array(
+                                'title_li' => NULL, 
+                                'child_of' => $findChildrenOf)); 
+                        ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+            <div class="generic-content">
+                <?php the_content(); ?>
+            </div>
+
+        </div>
         
 <?php }
 ?>
