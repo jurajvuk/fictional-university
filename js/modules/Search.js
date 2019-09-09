4d8ac6b59ad9f858348d63fc06bea9f2e4e39ac6
jurajvuk <jurajvuk@gmail.com>
2019-09-09 16:02:22 +0200

    sekcija 13 tutoriala započeta

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,6 +1,44 @@
+import $ from 'jquery';
+
 class Search {
+    // 1. Describe and create/initiate our object
     constructor() {
-        alert("hello, I am a search");
+        this.openButton = $(".js-search-trigger");
+        this.closeButton = $(".search-overlay__close");
+        this.searchOverlay = $(".search-overlay");
+        this.events();
+        this.isOverlayOpen = false;
+    }
+
+    // 2. Events
+    events() {
+        this.openButton.on("click", this.openOverlay.bind(this));
+        this.closeButton.on("click", this.closeOverlay.bind(this));
+        $(document).on("keydown", this.keyPressDispatcher.bind(this))
+    }
+    
+    // 3. Methods (function, action)
+    keyPressDispatcher(e) {
+        if (e.keyCode == 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+        if (e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
+    }
+    
+    openOverlay() {
+        this.searchOverlay.addClass("search-overlay--active");
+        $("body").addClass("body-no-scroll");
+        this.isOverlayOpen = true;
+        console.log("our open method just run");
+    }
+
+    closeOverlay() {
+        this.searchOverlay.removeClass("search-overlay--active");
+        $("body").removeClass("body-no-scroll");
+        this.isOverlayOpen = false;
+        console.log("our close method just run");
     }
 }
 
