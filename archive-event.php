0fd50fa3c1665a1b4d3d722b324e286043f77a35
jurajvuk <jurajvuk@gmail.com>
2019-10-21 15:58:45 +0200

    sekcija 13 tutoriala započeta

--- archive-event.php
+++ archive-event.php
@@ -13,3 +13,4 @@
             <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive</a>.</p>
         </div>
 <?php get_footer(); ?>
+
