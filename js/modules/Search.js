3e2f0e4997cbdad8b71e86874173dbadb8776014
jurajvuk <jurajvuk@gmail.com>
2019-10-25 15:45:21 +0200

    sekcija 14 tutoriala zapoceta

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -54,7 +54,7 @@ class Search {
         this.resultsDiv.html(`
           <h2 class="search-overlay__section-title">General Information</h2>
           ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general nformation matches that search.'}
-            ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+            ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> by ${item.author_name}</li>`).join('')}
           ${combinedResults.length ? '</ul>' : ''}
         `);
         this.isSpinnerVisible = false;
